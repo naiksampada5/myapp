b92d5c5122dde60c580490cc22cf473e1da5235c
Sampada Naik <naiksampada.5@gmail.com>
2020-06-23 22:22:04 +0530

    initial commit

---
+++ src/constants/About.js
@@ -0,0 +1,5 @@
+export const about = {
+    heading: "About Me",
+    "quote": "Look in the mirror. That's your competition.",
+    "bio": "A Computer Science Engineering student. Learning React-js,have knowledge about C,Java,Python and R. Like listening to music in free time.\n \n \n \n CONTACT DETAILS:\n Email:naiksampada.5@gmail.com \n Mob:9923246570 \n Git:https://github.com/naiksampada5 "
+};
---
+++ src/constants/Projects.js
@@ -0,0 +1,16 @@
+export const projects = {
+    heading: "Projects and Seminars",
+    quote: "Implementation of learning",
+    projectList: [
+        {
+            name: "[To-do App]",
+            description: "To-do app using React-js.With features of creating list, deleting list, and editing list.",
+            link: "https://github.com/naiksampada5/todo-app"
+        },
+        {
+            name: "[Ambient Intelligence]",
+            description: "A seminar on Ambient Intelligence(AmI).It consisted of what AmI is along with their technologies and  applications",
+            link: "https://github.com/naiksampada5/todo-app"
+        }
+    ]
+}
